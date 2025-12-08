2b8d1510532886c8c15dc20aef076278708858e2
bryansills <bryan@bryansills.com>
2025-12-07 21:59:08 -0600

    I think I have location working?

--- app/src/main/java/ninja/bryansills/adwaita/MainActivity.kt
+++ app/src/main/java/ninja/bryansills/adwaita/MainActivity.kt
@@ -1,23 +1,77 @@
 package ninja.bryansills.adwaita
 
 import android.location.Location
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.TextView
 import ninja.bryansills.adwaita.location.LocationProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private var _locationProvider: LocationProvider? = null
+    private val locationProvider: LocationProvider
+        get() = checkNotNull(_locationProvider) {
+            "You're trying to get location data, but the UI isn't visible..."
+        }
+
+    private var locationListener: LocationProvider.Listener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mainText = findViewById(R.id.hello_world) as TextView
-        mainText.text = DoesThisWork().showThis
 
-        val locationProvider = (this.application as AdwaitaApplication).locationProvider
-        locationProvider.addListener(object : LocationProvider.Listener {
-            override fun onLocationChanged(location: Location) {
-//                TODO("Not yet implemented")
+        _locationProvider = (this.application as AdwaitaApplication).locationProvider
+
+        if (!locationProvider.hasPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(
+                locationProvider.requiredPermissions,
+                LocationRequestCode
+            )
+        } else {
+            listenToLocation()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locationListener?.let { locationProvider.removeListener(it) }
+        locationListener = null
+        _locationProvider = null
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray,
+    ) {
+        when (requestCode) {
+            LocationRequestCode -> {
+                if (locationProvider.wasGrantedPermission(permissions, grantResults)) {
+                    listenToLocation()
+                } else {
+                    Log.w("BLARG", "User doesn't want us to see their location")
+                }
             }
-        })
+            else -> {
+                Log.w("BLARG", "Unknown request for permissions $requestCode")
+            }
+        }
+    }
+
+    private fun listenToLocation() {
+        val newListener = object : LocationProvider.Listener {
+            override fun onLocationChanged(location: Location) {
+                val mainText = findViewById(R.id.hello_world) as TextView
+                mainText.text = "${location.latitude}, ${location.longitude}"
+            }
+        }
+        locationProvider.addListener(newListener)
+        locationListener = newListener
+    }
+
+    companion object {
+        private const val LocationRequestCode = 123567
     }
 }
--- app/src/main/java/ninja/bryansills/adwaita/location/LocationProvider.kt
+++ app/src/main/java/ninja/bryansills/adwaita/location/LocationProvider.kt
@@ -2,9 +2,11 @@ package ninja.bryansills.adwaita.location
 
 import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
 import android.support.v4.content.PermissionChecker
 import android.support.v4.content.PermissionChecker.PERMISSION_GRANTED
 import android.util.Log
@@ -12,6 +14,11 @@ import android.util.Log
 interface LocationProvider {
 
     val hasPermission: Boolean
+
+    val requiredPermissions: Array<String>
+
+    fun wasGrantedPermission(permissions: Array<out String?>, grantResults: IntArray): Boolean
+
     fun addListener(listener: Listener)
 
     fun removeListener(listener: Listener)
@@ -36,10 +43,28 @@ class DefaultLocationProvider(
             return result == PERMISSION_GRANTED
         }
 
+    override val requiredPermissions: Array<String> = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+    override fun wasGrantedPermission(
+        permissions: Array<out String?>,
+        grantResults: IntArray
+    ): Boolean {
+        val locationIndex = permissions.indexOf(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        return if (locationIndex >= 0) {
+            val grantResult = grantResults[locationIndex]
+            grantResult == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+    }
+
     override fun addListener(listener: LocationProvider.Listener) {
-//        if (!hasPermission) {
-//            return // early exit!
-//        }
+        if (!hasPermission) {
+            return // early exit!
+        }
+
+        currentListeners.add(listener)
 
         // if we already have the location, send it
         lastLocation?.let { listener.onLocationChanged(it) }
@@ -47,21 +72,23 @@ class DefaultLocationProvider(
         // start listening to the system if we aren't already
         if (internalListener == null) {
             val newInternalListener = LocationListener { newLocation ->
-                currentListeners.forEach { it.onLocationChanged(newLocation) }
+                currentListeners.forEach {
+                    it.onLocationChanged(newLocation)
+                    lastLocation = newLocation
+                }
             }
-            val providerOptions = locationManager.allProviders
+
             val bestProvider = locationManager.bestProvider
-            Log.d("BLARG", providerOptions.joinToString())
 
             if (bestProvider != null) {
-//                locationManager.requestLocationUpdates(
-//                    bestProvider,
-//                    DurationBetweenUpdatesInMillis,
-//                    DistanceBetweenUpdatesInMeters,
-//                    newInternalListener
-//                )
+                locationManager.requestLocationUpdates(
+                    bestProvider,
+                    DurationBetweenUpdatesInMillis,
+                    DistanceBetweenUpdatesInMeters,
+                    newInternalListener
+                )
             } else {
-                Log.w("BLARG", "Cannot get location updates because device has no location providers")
+                Log.w("BLARG", "Cannot get location updates because device has no good location providers")
             }
 
             internalListener = newInternalListener
@@ -92,7 +119,14 @@ class DefaultLocationProvider(
 private val LocationManager.bestProvider: String?
     get() {
         val allOptions = this.getProviders(true)
-        return allOptions.find { it == LocationManager.FUSED_PROVIDER }
+
+        val bestOption = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            allOptions.find { it == LocationManager.FUSED_PROVIDER }
+        } else {
+            null
+        }
+
+        return bestOption
             ?: allOptions.find { it == LocationManager.NETWORK_PROVIDER }
             ?: allOptions.find { it == LocationManager.GPS_PROVIDER }
             ?: run {
